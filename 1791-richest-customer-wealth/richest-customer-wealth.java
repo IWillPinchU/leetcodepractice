f3806abe1f1bd0a02d03d39a3aeb7bf5496f31de
IWillPinchU <85113709+IWillPinchU@users.noreply.github.com>
2025-06-02 03:23:04 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (44.67%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,21 +1,13 @@
-///My first way of thinking not optimized
-
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int[] wealth = new int[accounts.length];
-        int counter = 0;
+        int ans = 0;
         for (int[] i: accounts){
             int money = 0;
             for(int j: i){
                 money += j;
             }
-            wealth[counter] = money;
-            counter++;
+            if (money > ans) ans = money;
         }
-        int rich = wealth[0];
-        for(int i = 1; i < wealth.length; i++){
-            if (wealth[i] > rich) rich = wealth[i];
-        }
-        return rich;
+        return ans;
     }
 }
