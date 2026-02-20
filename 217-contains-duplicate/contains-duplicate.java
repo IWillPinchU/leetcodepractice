cac531cc49a4f62770e60896286fcd126b8bf865
Sanskar Singhal <pinchusinghal6@gmail.com>
2026-02-20 16:09:58 +0530

    Time: 10 ms (99.32%) | Memory: 97.6 MB (14.90%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> meow = new HashSet<>();
         for(int num: nums){
-            if(meow.contains(num)) return true;
-            meow.add(num);
+            if(!meow.add(num)) return true;
         }
         return false;
     }
