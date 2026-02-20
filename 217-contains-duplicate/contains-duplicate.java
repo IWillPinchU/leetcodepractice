e8deec592a0f5711701b640147b3f2c3a1d61f3b
Sanskar Singhal <pinchusinghal6@gmail.com>
2026-02-20 16:17:04 +0530

    Time: 15 ms (65.06%) | Memory: 97.2 MB (15.51%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashMap<Integer,Integer> meowMap = new HashMap<>();
-        int i = 0;
         for(int num:nums){
             if(!meowMap.containsKey(num)) {
-                meowMap.put(num,i);
-                i++;
+                meowMap.put(num,0);
             }
             else return true;
         }
