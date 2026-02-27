03985720bbc0e5330741a8a70b5aad50f973cf72
Sanskar Singhal <pinchusinghal6@gmail.com>
2026-02-28 01:57:00 +0530

    Time: 14 ms (26.12%) | Memory: 46.7 MB (24.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,8 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> ht = new HashMap<>();
         if(s.length() != t.length()) return false;
-        for(int i = 0; i<s.length(); i++){
-            if(t.indexOf(s.charAt(i)) != -1) t = t.replaceFirst(String.valueOf(s.charAt(i)),"");
+
+        for(char c:s.toCharArray()){
+            ht.put(c,ht.getOrDefault(c,0) + 1);
+        }
+
+        for(char c:t.toCharArray()){
+            if(ht.containsKey(c)){
+                ht.put(c,ht.getOrDefault(c,0) - 1);
+                if(ht.get(c) == 0) ht.remove(c);
+            }
             else return false;
         }
         return true;
