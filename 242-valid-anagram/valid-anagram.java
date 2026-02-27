0a2c89abec4ee3fab3ed51d6735bbbd705237ed3
Sanskar Singhal <pinchusinghal6@gmail.com>
2026-02-28 01:42:14 +0530

    Time: 620 ms (5.37%) | Memory: 48.1 MB (8.59%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        for(int i = 0; i<s.length(); i++){
+            if(t.indexOf(s.charAt(i)) != -1) t = t.replaceFirst(String.valueOf(s.charAt(i)),"");
+            else return false;
+        }
+        return true;
+    }
+}
