306c1787651de07b4d159efb11f094bab6d36251
Sanskar Singhal <pinchusinghal6@gmail.com>
2026-03-02 17:27:59 +0530

    Time: 4 ms (50.01%) | Memory: 44.5 MB (57.75%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -7,13 +7,17 @@ class Solution {
             return true;
         }
         while(i<j){
-            if(Character.isLetterOrDigit(s.charAt(i)) && Character.isLetterOrDigit(s.charAt(j))){
-                if(s.charAt(i) != s.charAt(j)) return false;
+            if(!Character.isLetterOrDigit(s.charAt(i))){
                 i++;
-                j--;
+                continue;
             }
-            else if(!Character.isLetterOrDigit(s.charAt(i))) i++;
-            else if(!Character.isLetterOrDigit(s.charAt(j))) j--;
+            if(!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+                continue;
+            }
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
+            i++;
+            j--;
         }
         return true;
     }
