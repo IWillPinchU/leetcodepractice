586ab02b37a0701b9d6ce9c681408a9a5006794a
Sanskar Singhal <pinchusinghal6@gmail.com>
2026-03-04 21:48:44 +0530

    Time: 10 ms (99.45%) | Memory: 49.4 MB (19.64%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.java
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -2,24 +2,22 @@ class Solution {
     public String compressedString(String word) {
         char[] string = word.toCharArray();
         char[] result = new char[2 * string.length];
-        int count = 0;
         int idx = 0;
         char last = string[0];
-        for(int i = 0; i<string.length; i++){
+        int count = 1;
+        for(int i = 1; i<string.length; i++){
             if(count<9 && string[i] == last){
                 count++;
-                last = string[i];
-                continue;
             }
-            result[idx] = (char) ('0' + count);
-            result[idx+1] = last;
-            idx += 2;
-            count = 1;
-            last = string[i];
+            else{
+                result[idx++] = (char) ('0' + count);
+                result[idx++] = last;
+                count = 1;
+                last = string[i];
+            }
         }
-        result[idx] = (char) ('0' + count);
-        result[idx + 1] = last;
-        idx+=2;
+        result[idx++] = (char) ('0' + count);
+        result[idx++] = last;
         return new String(result, 0, idx);
     }
 }
