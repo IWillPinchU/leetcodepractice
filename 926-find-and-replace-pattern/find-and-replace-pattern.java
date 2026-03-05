7a9a504b6768ec5cec3256796a74891633026ab6
Sanskar Singhal <pinchusinghal6@gmail.com>
2026-03-05 20:23:00 +0530

    Time: 1 ms (77.12%) | Memory: 43.3 MB (99.28%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.java
+++ 926-find-and-replace-pattern/find-and-replace-pattern.java
@@ -2,23 +2,11 @@ class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         List<String> result = new ArrayList<>();
         for(String s:words){
-            if(s.length() != pattern.length()) break;
-            char[] sMap = new char[26];
-            char[] patternMap = new char[26];
-            boolean flag = true;
+            int flag = 1;
             for(int i = 0; i<s.length(); i++){
-                char c1 = s.charAt(i);
-                char c2 = pattern.charAt(i);
-                if(sMap[c1 - 'a'] == 0 && patternMap[c2  - 'a'] == 0){
-                    sMap[c1  - 'a'] = c2;
-                    patternMap[c2  - 'a'] = c1;
-                }
-                else if(sMap[c1  - 'a'] != c2 || patternMap[c2  - 'a'] != c1){
-                    flag = false;
-                    break;
-                }
+                if(s.indexOf(s.charAt(i)) != pattern.indexOf(pattern.charAt(i))) flag = 0;
             }
-            if(flag) result.add(s);
+            if(flag == 1) result.add(s);
         }
         return result;
     }
