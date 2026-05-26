4909cd172641abc94aefd00518a164afe98efc06
Sanskar Singhal <pinchusinghal6@gmail.com>
2026-05-26 22:14:40 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (87.02%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,28 +1,33 @@
 class Solution {
     public int myAtoi(String s) {
+        if(s.length() == 0) return 0;
+
         char[] stringy = s.toCharArray();
-        long result = 0;
         int i = 0;
         int flag = 1;
-        if(s.length() == 0) return 0;
         for(; i<stringy.length; i++){
             if(stringy[i] == ' ') continue;
             else break;
         }
+
         if(i < s.length() && (stringy[i] == '-' || stringy[i] == '+')){
             if (stringy[i] == '-') flag = -1;
             i++;
         }
-        for(;i<stringy.length;i++){
-            if(Character.isDigit(stringy[i])){
-                result = result * 10 + stringy[i] - '0';
-                if(result > Integer.MAX_VALUE || result < Integer.MIN_VALUE){
-                    if (flag == -1) return Integer.MIN_VALUE;
-                    else return Integer.MAX_VALUE;
-                }
-            }
-            else break;
+
+        return (int)myAtoiRec(stringy, i, flag, 0);
+    }
+
+    public long myAtoiRec(char[] stringy, int i, int flag, long numberSoFar){
+        if(i == stringy.length || !Character.isDigit(stringy[i])) return numberSoFar * flag;
+
+        numberSoFar = numberSoFar * 10 + stringy[i] - '0';
+
+        if(numberSoFar > Integer.MAX_VALUE){
+            if (flag == -1) return Integer.MIN_VALUE;
+            else return Integer.MAX_VALUE;
         }
-        return (int)result * flag;
+
+        return myAtoiRec(stringy, ++i, flag, numberSoFar);
     }
 }
